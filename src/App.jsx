81f0cb0235244663f573cbf28d0901ed39d2d78a
akbaradiw <akbaradiw000@gmail.com>
2024-10-07 18:14:37 +0700

    go

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+
+function App() {
+
+  return (
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  
+  )
+}
+
+export default App
